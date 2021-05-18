fb298eb69c04e6e643de5c67a8128e87a081f466
MusicPup13 <57156846+MusicPup13@users.noreply.github.com>
2021-05-17 22:26:07 -0400

    Add files via upload

---
+++ docs/exercises/Exercise01/Build/Scripts/Igloo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Igloo : MonoBehaviour
+{
+    public GameObject myPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(myPrefab, new Vector3(57, 0, 18), Quaternion.identity);
+
+        Instantiate(myPrefab, new Vector3(5, 0, 64), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
