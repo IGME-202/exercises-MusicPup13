d61f4564f97033044f20ae32167e6eab75e9a6dc
MusicPup13 <57156846+MusicPup13@users.noreply.github.com>
2021-05-17 22:32:11 -0400

    Add files via upload

---
+++ docs/exercises/Exercise07/Build/Scripts/SceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject small;
+    public GameObject medium;
+    public GameObject large;
+
+    public float mass;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sets masses
+        GameObject.Find("small").GetComponent<Vehicle>().mass = 1;
+        GameObject.Find("medium").GetComponent<Vehicle>().mass = 5;
+        GameObject.Find("large").GetComponent<Vehicle>().mass = 20;
+
+        GameObject[] monsters = new GameObject[3];  //array for the objects
+
+        //instantiates objects and puts them in the array
+        monsters[0] = GameObject.Instantiate(small, new Vector3(10f, -20f, 0f), Quaternion.identity) as GameObject;
+        monsters[1] = GameObject.Instantiate(medium, new Vector3(-10f, 40f, 0f), Quaternion.identity) as GameObject;
+        monsters[2] = GameObject.Instantiate(large, new Vector3(15f, 60f, 0f), Quaternion.identity) as GameObject;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ docs/exercises/Exercise07/Build/Scripts/Vehicle.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vehicle : MonoBehaviour
+{
+    //inspector values
+    private Vector3 vehiclePosition;
+    private Vector3 direction = Vector3.up;
+    private Vector3 velocity;
+    private Vector3 acceleration;
+    private bool frictionOn;
+
+    public float totalRotation;
+    public float angleDelta = 1;
+    public float coefFric = 1;
+
+    public float mass;
+
+    public float walkForce;
+    public Vector3 gravForce = new Vector3(0f, -1f, 0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vehiclePosition = transform.position;
+        frictionOn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        acceleration = Vector3.zero;
+
+        ApplyForce(gravForce);
+
+        //turning friction on and off
+        if (Input.GetKeyDown(KeyCode.F) == true)
+        {
+            if (frictionOn == true)
+            {
+                frictionOn = false;
+            }
+            else
+            {
+                frictionOn = true;
+            }
+        }
+
+        if (frictionOn == true)
+        {
+            ApplyFriction(coefFric);
+        }
+
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            ApplyMouseForce();
+        }
+
+        TurnVehicle();
+        UpdatePostion();
+        Bounce();
+        SetTransform();
+    }
+
+    void TurnVehicle()
+    {
+        //left rotation with left arrow key
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = Quaternion.Euler(0, 0, angleDelta) * direction;
+            totalRotation += angleDelta;
+        }
+
+        //right rotation with right arrow key
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = Quaternion.Euler(0, 0, -angleDelta) * direction;
+            totalRotation -= angleDelta;
+        }
+    }
+
+    void UpdatePostion()
+    {
+        //Move vehicle forward on key press
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            ApplyForce(direction * walkForce);
+        }
+
+        velocity += acceleration * Time.deltaTime;
+
+        //use velocity to change object's position
+        vehiclePosition += velocity * Time.deltaTime;
+    }
+
+    void WrapVehicle()
+    {
+        Camera cam = Camera.main;
+        Vector3 max = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, cam.nearClipPlane));
+        Vector3 min = cam.ScreenToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
+
+        //position vehicle back on screen if it's off screen
+        if (vehiclePosition.x > max.x)
+        {
+            vehiclePosition.x = min.x;
+        }
+
+        if (vehiclePosition.x < min.x)
+        {
+            vehiclePosition.x = max.x;
+        }
+
+        if (vehiclePosition.y > max.y)
+        {
+            vehiclePosition.y = min.y;
+        }
+
+        if (vehiclePosition.y < min.y)
+        {
+            vehiclePosition.y = max.y;
+        }
+    }
+
+    void SetTransform()
+    {
+        //Update rotation
+        transform.rotation = Quaternion.Euler(0, 0, totalRotation);
+
+        //update position
+        transform.position = vehiclePosition;
+    }
+
+    void ApplyForce(Vector3 force)
+    {
+        acceleration += force / mass;
+    }
+
+    void ApplyFriction(float coeff)
+    {
+        //creates a friction force and applys it
+        Vector3 friction = velocity * -1;
+        friction.Normalize();
+        friction = friction * coeff;
+        acceleration += friction;
+    }
+
+    void ApplyMouseForce()
+    {
+        //gets mouse position when mouse is pressed and applys a force to the objects from it
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        velocity -= mouseWorldPos;
+        acceleration += velocity;
+    }
+
+    void Bounce()
+    {
+        Camera cam = Camera.main;
+        Vector3 max = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, cam.nearClipPlane));
+        Vector3 min = cam.ScreenToWorldPoint(new Vector3(0, 0, cam.nearClipPlane));
+
+        //position vehicle back on screen if it's off screen
+        if (vehiclePosition.x > max.x)
+        {
+            velocity.x *= -1;
+        }
+
+        if (vehiclePosition.x < min.x)
+        {
+            velocity.x *= -1;
+        }
+
+        if (vehiclePosition.y > max.y)
+        {
+            velocity.y *= -1;
+        }
+
+        if (vehiclePosition.y < min.y)
+        {
+            velocity.y *= -1;
+        }
+    }
+}
